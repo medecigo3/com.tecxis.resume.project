54461bd4a2fc21ae0ac3b31fefc96a546a74e482
medec <medec@Surface>
2018-11-21 22:05:36 +0100

    Impl. Client get Contracts unit test

--- data/src/test/java/com/tecxis/resume/ClientTest.java
+++ data/src/test/java/com/tecxis/resume/ClientTest.java
@@ -1,13 +1,40 @@
 package com.tecxis.resume;
 
-import static org.junit.Assert.*;
+import static com.tecxis.resume.persistence.ClientRepositoryTest.AGEAS;
+import static com.tecxis.resume.persistence.ContractRepositoryTest.CONTRACT12_STARTDATE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.tecxis.resume.persistence.ClientRepository;
+import com.tecxis.resume.persistence.ContractRepository;
 
 public class ClientTest {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private ClientRepository clientRepo;
+	
+	@Autowired
+	private  ContractRepository contractRepo;
 
 	@Test
 	public void testGetClientId() {
@@ -25,8 +52,19 @@ public class ClientTest {
 	}
 
 	@Test
+	@Sql(
+		scripts= {"classpath:SQL/DropResumeSchema.sql", "classpath:SQL/CreateResumeSchema.sql", "classpath:SQL/CreateResumeData.sql" },
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)	
 	public void testGetContracts() {
-		fail("Not yet implemented");
+		/**Get client*/
+		Client ageas = clientRepo.getClientByName(AGEAS);
+		/**Get client contracts*/
+		List<Contract> ageasContracts = ageas.getContracts();
+		assertEquals(1, ageasContracts.size());
+		/**Compare with fetched contract*/
+		Contract ageasContract = contractRepo.getContractByStartDate(CONTRACT12_STARTDATE);
+		assertNotNull(ageasContract);
+		assertEquals(ageasContract, ageasContracts.get(0));
 	}
 
 	@Test
