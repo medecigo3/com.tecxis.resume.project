0571474fa7c440d6828e5c681c30076a3234988e
medec <medec@Surface>
2019-02-22 18:44:52 +0100

    Impl. Supplier GetContracts unit test

--- data/src/test/java/com/tecxis/resume/SupplierTest.java
+++ data/src/test/java/com/tecxis/resume/SupplierTest.java
@@ -1,13 +1,54 @@
 package com.tecxis.resume;
 
-import static org.junit.Assert.*;
+import static com.tecxis.resume.persistence.ClientRepositoryTest.*;
+import static com.tecxis.resume.persistence.StaffRepositoryTest.AMT_NAME;
+import static com.tecxis.resume.persistence.SupplierRepositoryTest.ACCENTURE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tecxis.resume.persistence.ClientRepository;
+import com.tecxis.resume.persistence.ClientRepositoryTest;
+import com.tecxis.resume.persistence.ContractRepository;
+import com.tecxis.resume.persistence.StaffRepository;
+import com.tecxis.resume.persistence.SupplierRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringJUnitConfig (locations = { 
+		"classpath:persistence-context.xml", 
+		"classpath:test-dataSource-context.xml",
+		"classpath:test-transaction-context.xml" })
+@Commit
+@Transactional(transactionManager = "transactionManager", isolation = Isolation.READ_UNCOMMITTED)
 public class SupplierTest {
+	
+	@Autowired
+	private  ContractRepository contractRepo;
+	
+	@Autowired
+	private StaffRepository staffRepo;
+	
+	@Autowired
+	private SupplierRepository supplierRepo;
+	
+	@Autowired
+	private ClientRepository clientRepo;
 
 	@Test
 	public void testGetSupplierId() {
@@ -25,8 +66,40 @@ public class SupplierTest {
 	}
 
 	@Test
+	@Sql(
+		scripts= {"classpath:SQL/DropResumeSchema.sql", "classpath:SQL/CreateResumeSchema.sql", "classpath:SQL/CreateResumeData.sql"},
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
 	public void testGetContracts() {
-		fail("Not yet implemented");
+		/**Find a Supplier*/
+		Staff amt = staffRepo.getStaffLikeName(AMT_NAME);
+		Supplier accenture = supplierRepo.getSupplierByNameAndStaff(ACCENTURE, amt);
+		
+		/**Find Clients*/
+		Client barclays = clientRepo.getClientByName(BARCLAYS);
+		Client ageas = clientRepo.getClientByName(AGEAS);
+		Client accentureClient = clientRepo.getClientByName(ClientRepositoryTest.ACCENTURE);
+		assertEquals(BARCLAYS, barclays.getName());
+		assertEquals(AGEAS, ageas.getName());
+		assertEquals(ClientRepositoryTest.ACCENTURE, accentureClient.getName());
+					
+		/**Verify the Supplier*/
+		assertEquals(ACCENTURE, accenture.getName());				
+		
+		/**Check Contracts*/
+		List <Contract> amesysContracts = accenture.getContracts();
+		assertEquals(3, amesysContracts.size());		
+		
+		List <Contract> accentureBarclaysContracts = contractRepo.findByClientAndSupplierOrderByStartDateAsc(barclays, accenture);
+		List <Contract> accentureAgeasContracts = contractRepo.findByClientAndSupplierOrderByStartDateAsc(ageas, accenture);
+		List <Contract> accentureAccentureContracts = contractRepo.findByClientAndSupplierOrderByStartDateAsc(accentureClient, accenture);		
+		assertEquals(1, accentureBarclaysContracts.size());				
+		assertEquals(1, accentureAgeasContracts.size());		
+		assertEquals(1, accentureAccentureContracts.size());
+		Contract accentureBarclaysContract = accentureBarclaysContracts.get(0);
+		Contract accentureAgeasContract =   accentureAgeasContracts.get(0);
+		Contract accentureAccentureContract = accentureAccentureContracts.get(0);
+		
+		assertThat(amesysContracts,  Matchers.containsInAnyOrder(accentureBarclaysContract,  accentureAgeasContract,  accentureAccentureContract));
 	}
 
 	@Test
