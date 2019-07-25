0f228b6c1a95211330347b4295b26afaf6756008
Arturo <Arturo@LAPTOP-ASUS>
2019-07-25 16:27:50 +0200

    Impl. EnrolmentRepository find all unit test

--- data/src/test/java/com/tecxis/resume/persistence/EnrolmentRepositoryTest.java
+++ data/src/test/java/com/tecxis/resume/persistence/EnrolmentRepositoryTest.java
@@ -1,16 +1,52 @@
 package com.tecxis.resume.persistence;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tecxis.resume.Enrolment;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringJUnitConfig (locations = { 
+		"classpath:persistence-context.xml", 
+		"classpath:test-dataSource-context.xml",
+		"classpath:test-transaction-context.xml" })
+@Commit
+@Transactional(transactionManager = "transactionManager", isolation = Isolation.READ_UNCOMMITTED)
 public class EnrolmentRepositoryTest {
 	
 	final public static String ENROLMENT_TABLE = "ENROLMENT";
-
+	
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
+	@Autowired 
+	private EnrolmentRepository enrolmentRepo;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Sql(
+		scripts= {"classpath:SQL/DropResumeSchema.sql", "classpath:SQL/CreateResumeSchema.sql", "classpath:SQL/CreateResumeData.sql" },
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
+	public void testFindAll(){
+		List <Enrolment> allEnrollments = enrolmentRepo.findAll();
+		assertEquals(1, allEnrollments.size());
+		
 	}
 
 }
