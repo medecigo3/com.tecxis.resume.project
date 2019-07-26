f3c387b945c0afc1c987a9d9321bdc900f2838ad
Arturo <Arturo@LAPTOP-ASUS>
2019-07-26 14:34:03 +0200

    Impl. Enrolment getStaffs unit test

--- data/src/test/java/com/tecxis/resume/EnrolmentTest.java
+++ data/src/test/java/com/tecxis/resume/EnrolmentTest.java
@@ -4,10 +4,10 @@ import static com.tecxis.resume.persistence.CourseRepositoryTest.BW_6_COURSE;
 import static com.tecxis.resume.persistence.CourseRepositoryTest.COURSE_TABLE;
 import static com.tecxis.resume.persistence.CourseRepositoryTest.SHORT_BW_6_COURSE;
 import static com.tecxis.resume.persistence.EnrolmentRepositoryTest.ENROLMENT_TABLE;
-import static com.tecxis.resume.persistence.StaffRepositoryTest.AMT_LASTNAME;
+import static com.tecxis.resume.persistence.StaffRepositoryTest.*;
 import static com.tecxis.resume.persistence.StaffRepositoryTest.AMT_NAME;
 import static com.tecxis.resume.persistence.StaffRepositoryTest.STAFF_TABLE;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTable;
 
 import java.util.List;
@@ -55,6 +55,69 @@ public class EnrolmentTest {
 	
 	@Autowired 
 	private CourseRepository courseRepo;
+	
+	
+	@Test
+	@Sql(
+		scripts= {"classpath:SQL/DropResumeSchema.sql", "classpath:SQL/CreateResumeSchema.sql", "classpath:SQL/CreateResumeData.sql" },
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
+	public void testSetStaffs() {		
+		/**Find Staff*/
+		Staff amt = staffRepo.getStaffLikeLastname(AMT_LASTNAME);
+		assertEquals(AMT_NAME, amt.getName());
+		assertEquals(AMT_LASTNAME , amt.getLastname());
+		
+		/**Find Course*/
+		List <Course> courses = courseRepo.getCourseLikeTitle(SHORT_BW_6_COURSE);
+		assertEquals(1, courses.size());
+		Course bwCourse = courses.get(0);
+		assertEquals(BW_6_COURSE, bwCourse.getTitle());
+		
+		/**Find Enrolment to update*/
+		Enrolment bwEnrolment = enrolmentRepo.findById(new EnrolmentId(amt, bwCourse)).get();
+		assertEquals(amt, bwEnrolment.getEnrolmentId().getStaff());
+		assertEquals(bwCourse, bwEnrolment.getEnrolmentId().getCourse());
+				
+		/**Find Staff to set in the Course*/		
+		Staff john = staffRepo.getStaffByNameAndLastname(JOHN_NAME, JOHN_LASTNAME);
+		assertNotNull(john);
+		assertEquals(JOHN_NAME, john.getName());
+		assertEquals(JOHN_LASTNAME , john.getLastname());
+		
+		
+		/**Create new Enrolment*/		
+		Enrolment newEnrolment = new Enrolment(new EnrolmentId(john, bwCourse));
+		
+		/**Verify initial state*/
+		assertEquals(1, countRowsInTable(jdbcTemplate, ENROLMENT_TABLE));
+		assertEquals(1, countRowsInTable(jdbcTemplate, COURSE_TABLE));		
+		assertEquals(2, countRowsInTable(jdbcTemplate, STAFF_TABLE));
+		
+		/**Set new Staff*/		
+		entityManager.remove(bwEnrolment);
+		entityManager.persist(newEnrolment);		
+		entityManager.flush();
+		entityManager.clear();		
+		
+		assertEquals(1, countRowsInTable(jdbcTemplate, ENROLMENT_TABLE));
+		assertEquals(1, countRowsInTable(jdbcTemplate, COURSE_TABLE));		
+		assertEquals(2, countRowsInTable(jdbcTemplate, STAFF_TABLE));
+		
+		/**Find old Enrolment*/
+		assertFalse(enrolmentRepo.findById(new EnrolmentId(amt, bwCourse)).isPresent());
+		/**Find new Enrolment*/
+		assertTrue(enrolmentRepo.findById(new EnrolmentId(john, bwCourse)).isPresent());
+	}
+	
+	@Test
+	public void testAddStaff() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRemoveStaff() {
+		fail("Not yet implemented");
+	}
 	
 	@Test
 	@Sql(
