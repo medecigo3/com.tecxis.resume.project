49bc6045f4a4c0cc098236cc4749cb812dffde0f
Arturo Medecigo <amt.market1@gmail.com>
2020-11-12 08:39:28 +0100

    Adds CityIdTest to source control

---
+++ data/src/test/java/com/tecxis/commons/persistence/id/CityIdTest.java
@@ -0,0 +1,13 @@
+package com.tecxis.commons.persistence.id;
+
+import org.junit.Test;
+
+public class CityIdTest {
+
+	@Test
+	public void testToString() {
+		CityId cityId = new CityId();
+		cityId.toString();
+	}
+
+}
