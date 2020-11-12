692a7e711869d0dca97caf4c184ab8a89cee8d51
Arturo Medecigo <amt.market1@gmail.com>
2020-11-12 08:34:41 +0100

    Adds ContractIdTest to source control

---
+++ data/src/test/java/com/tecxis/commons/persistence/id/ContractIdTest.java
@@ -0,0 +1,13 @@
+package com.tecxis.commons.persistence.id;
+
+import org.junit.Test;
+
+public class ContractIdTest {
+
+	@Test
+	public void testToString() {
+		ContractId contractId = new ContractId();
+		contractId.toString();
+	}
+
+}
