c9633f2efbe5221836490abdad3b504628b277be
Arturo Medecigo <amt.market1@gmail.com>
2021-04-30 11:47:03 +0200

    Prepare to move the package 'com.tecxis.resume.domain.repository' to 'resume-persistence-jpa' module: move constants to Tables.class

--- resume-domain-jpa/src/test/java/com/tecxis/resume/domain/constants/Tables.java
+++ resume-domain-jpa/src/test/java/com/tecxis/resume/domain/constants/Tables.java
@@ -2,7 +2,7 @@ package com.tecxis.resume.domain.constants;
 
 public class Tables {
 
-	public static final String CONTRACT_SERVICE_AGREEMENT_TABLE = "CONTRACT_SERVICE_AGREEMENT";
+	final public static String CONTRACT_SERVICE_AGREEMENT_TABLE = "CONTRACT_SERVICE_AGREEMENT";
 	final public static String STAFF_SKILL_TABLE = "STAFF_SKILL";
 
 	private Tables() {
