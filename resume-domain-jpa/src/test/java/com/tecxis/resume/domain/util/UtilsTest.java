9f11f9de685ada78cc2e8523a7c69ca35533672c
Arturo Medecigo <amt.market1@gmail.com>
2021-09-11 23:56:25 +0200

    Uncomments UtilsTest.testInsertEmploymentContractWithSpringJpaRepo()

--- resume-domain-jpa/src/test/java/com/tecxis/resume/domain/util/UtilsTest.java
+++ resume-domain-jpa/src/test/java/com/tecxis/resume/domain/util/UtilsTest.java
@@ -321,10 +321,10 @@ public class UtilsTest {
 		assertEquals(1, countRowsInTable(jdbcTemplate, EMPLOYMENT_CONTRACT_TABLE));
 	}
 	
-//	@Test
-//	@Sql(
-//		scripts= {"classpath:SQL/H2/DropResumeSchema.sql", "classpath:SQL/H2/CreateResumeSchema.sql"},
-//		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
+	@Test
+	@Sql(
+		scripts= {"classpath:SQL/H2/DropResumeSchema.sql", "classpath:SQL/H2/CreateResumeSchema.sql"},
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
 	public void testInsertEmploymentContractWithSpringJpaRepo() {
 		/**Prepare test*/
 		Supplier alterna = Utils.insertASupplier(ALTERNA,  supplierRepo);			
