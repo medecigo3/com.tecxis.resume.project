dc54596a9187999f86dcdb833a7dab824ac413cb
Arturo Medecigo <amt.market1@gmail.com>
2021-09-11 23:54:22 +0200

    Re-generates Location equals() & hashCode() using only LocationId

--- resume-domain-jpa/src/main/java/com/tecxis/resume/domain/Location.java
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/Location.java
@@ -81,43 +81,32 @@ public class Location implements Serializable {
 	public void setId(LocationId id) {
 		this.id = id;
 	}
-
-	@Override
-	public boolean equals(Object other) {
-		if (this == other) {
-			return true;
-		}
-		if (!(other instanceof Location)) {
-			return false;
-		}
-		Location castOther = (Location)other;
-		
-		if(this.getCity() != null && castOther.getCity() != null) {
-			if (this.getProject() != null && castOther.getProject() != null) {
-				
-				 return 	this.getCity().equals(castOther.getCity()) &&
-						 	this.getProject().equals(castOther.getProject());
-				 
-			} else return false;
-		} else return false;
-			
-	}
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int hash = 17;
-		
-		if (this.getCity() != null) {
-			hash = hash * prime + this.getCity().hashCode();
-		}
-		
-		if (this.getProject() != null) {
-			hash = hash * prime + this.getProject().hashCode();
-		}		
-		return hash;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {			
 		return "["+ this.getClass().getName() +
