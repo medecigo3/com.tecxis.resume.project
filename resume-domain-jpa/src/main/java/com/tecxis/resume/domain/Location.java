8fb977b017f50c2a3f313cd43e218d34cb0b1980
Arturo Medecigo <amt.market1@gmail.com>
2021-09-12 22:40:31 +0200

    Refactors Location.hashCode()

--- resume-domain-jpa/src/main/java/com/tecxis/resume/domain/Location.java
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/Location.java
@@ -84,10 +84,7 @@ public class Location implements Serializable {
 	
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return this.getId().hashCode();
 	}
 
 	@Override
