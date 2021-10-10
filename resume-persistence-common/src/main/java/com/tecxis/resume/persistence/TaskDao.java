a993c51ca9db6af4db87a5bcdcc5bc783fc65871
Arturo Medecigo <amt.market1@gmail.com>
2021-10-10 18:02:51 +0200

    Renames AssignmentDao into TaskDao and vice-versa

--- resume-persistence-common/src/main/java/com/tecxis/resume/persistence/TaskDao.java
+++ resume-persistence-common/src/main/java/com/tecxis/resume/persistence/TaskDao.java
@@ -2,13 +2,12 @@ package com.tecxis.resume.persistence;
 
 import java.util.List;
 
-import com.tecxis.resume.domain.Assignment;
 import com.tecxis.resume.domain.Task;
 
 public interface TaskDao extends Dao<Task> {
 
-	public List <Assignment> getTaskLikeDesc(String name);
+	public List <Task> getTaskLikeDesc(String name);
 
-	public Assignment getTaskByDesc(String name);
+	public Task getTaskByDesc(String name);
 
 }
