16ccf9a4d9b422332a5e3479b9ceb1c01a16ea76
Arturo <Arturo@LAPTOP-ASUS>
2021-10-16 23:17:16 +0200

    [Agreement, AgreementDao] unit test set/update Contract

--- resume-domain-jpa/src/test/java/com/tecxis/resume/domain/AgreementTest.java
+++ resume-domain-jpa/src/test/java/com/tecxis/resume/domain/AgreementTest.java
@@ -2,6 +2,7 @@ package com.tecxis.resume.domain;
 
 import static com.tecxis.resume.domain.Constants.AXELTIS;
 import static com.tecxis.resume.domain.Constants.CONTRACT7_NAME;
+import static com.tecxis.resume.domain.Constants.CONTRACT1_NAME;
 import static com.tecxis.resume.domain.Constants.FASTCONNECT;
 import static com.tecxis.resume.domain.Constants.LIFERAY_DEVELOPPER;
 import static com.tecxis.resume.domain.Constants.TIBCO_BW_CONSULTANT;
@@ -89,11 +90,11 @@ public class AgreementTest {
 		/***Create new Agreement*/
 		AgreementId newAxeltisFastConnectAgreementId = new AgreementId();
 		newAxeltisFastConnectAgreementId.setContractId(axeltisFastConnectcontract.getId());
-		newAxeltisFastConnectAgreementId.setServiceId(liferayDev.getId());
+		newAxeltisFastConnectAgreementId.setServiceId(liferayDev.getId()); // set new service id
 		Agreement newAxeltisFastConnectAgreement = new Agreement();
 		newAxeltisFastConnectAgreement.setId(newAxeltisFastConnectAgreementId);
 		newAxeltisFastConnectAgreement.setContract(axeltisFastConnectcontract);
-		newAxeltisFastConnectAgreement.setService(liferayDev);
+		newAxeltisFastConnectAgreement.setService(liferayDev); // set new service
 		
 		/**Verify initial state*/
 		SchemaUtils.testInitialState(jdbcTemplateProxy);
@@ -107,10 +108,63 @@ public class AgreementTest {
 		/**Verify post state*/
 		SchemaUtils.testInitialState(jdbcTemplateProxy);
 		/**Test changes*/
-		/**Find old Agreement*/
+		/**Find new Agreement*/
 		assertNotNull(agreementRepo.findByContractAndService(axeltisFastConnectcontract, liferayDev));
-		/**Find new Enrolment*/
+		/**Find old Enrolment*/
 		assertFalse(agreementRepo.findById(new AgreementId(axeltisFastConnectcontract.getId(), tibcoCons.getId())).isPresent());	
+	}
+	
+	@Test()
+	@Sql(
+		scripts= {"classpath:SQL/H2/DropResumeSchema.sql", "classpath:SQL/H2/CreateResumeSchema.sql", "classpath:SQL/InsertResumeData.sql" },
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)	
+	public void testSetContract() {
+		/**Find Client*/
+		Client axeltis = clientRepo.getClientByName(AXELTIS);
+		assertEquals(AXELTIS, axeltis.getName());		
+		
+		/**Find supplier*/
+		Supplier fastconnect = supplierRepo.getSupplierByName(FASTCONNECT);
+		assertEquals(FASTCONNECT, fastconnect.getName());	
+		
+		/**Find Contract*/
+		Contract axeltisFastConnectcontract = contractRepo.getContractByName(CONTRACT7_NAME);
+		
+		/**Find Service*/
+		Service tibcoCons = serviceRepo.getServiceByName(TIBCO_BW_CONSULTANT);
+		
+		/**Find target Agreement to remove*/
+		Agreement axeltisFastConnectAgreement = agreementRepo.findById(new AgreementId(axeltisFastConnectcontract.getId(), tibcoCons.getId())).get();
+		
+		/**Find new Contract to set in Agreement*/
+		Contract accentureBarclaysContract = contractRepo.getContractByName(CONTRACT1_NAME);
+		
+		/***Create new Agreement*/
+		AgreementId newAxeltisFastConnectAgreementId = new AgreementId();
+		newAxeltisFastConnectAgreementId.setContractId(accentureBarclaysContract.getId()); //set new contract id
+		newAxeltisFastConnectAgreementId.setServiceId(tibcoCons.getId());
+		Agreement newAxeltisFastConnectAgreement = new Agreement();
+		newAxeltisFastConnectAgreement.setId(newAxeltisFastConnectAgreementId);
+		newAxeltisFastConnectAgreement.setContract(accentureBarclaysContract); // set new contract
+		newAxeltisFastConnectAgreement.setService(tibcoCons);
+		
+		/**Verify initial state*/
+		SchemaUtils.testInitialState(jdbcTemplateProxy);
+		
+		/**Remove old and create new Agreement*/
+		entityManager.remove(axeltisFastConnectAgreement);
+		entityManager.persist(newAxeltisFastConnectAgreement);
+		entityManager.flush();
+		entityManager.clear();	
+		
+		/**Verify post state*/
+		SchemaUtils.testInitialState(jdbcTemplateProxy);
+		/**Test changes*/
+		/**Find new Agreement*/
+		assertNotNull(agreementRepo.findByContractAndService(accentureBarclaysContract, tibcoCons));
+		/**Find old Enrolment*/
+		assertFalse(agreementRepo.findById(new AgreementId(axeltisFastConnectcontract.getId(), tibcoCons.getId())).isPresent());
+		
 	}
 	
 	@Test()
