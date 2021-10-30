0b917e2261c9fae62f849927ce165d1ab55962e0
Arturo <Arturo@LAPTOP-ASUS>
2021-10-30 17:51:50 +0200

    Intro. functional testing interfaces
    + AgremeentValidator: Agreement functional validator
    + JpaRepositoryTransactionVoidFunction: introduces function "void JPA
    transaction with generic JpaRepository"
    + SetContractAgreementFunction: introduces generic function definition
    to "set contract agreement". Where generic type is a EntityManager,
    JpaRepository, Hibernate, JDBC or other transaction manager equivalent
    type.

---
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/AgreementValidator.java
@@ -0,0 +1,33 @@
+package com.tecxis.resume.domain.util.function;
+
+import java.util.function.Function;
+
+import com.tecxis.resume.domain.Agreement;
+import com.tecxis.resume.domain.util.function.AgreementValidator.AgreementValidationResult;
+
+public interface AgreementValidator extends Function <Agreement, AgreementValidationResult> {
+	
+
+	static AgreementValidator isContractValid(String contractName) {
+		return agreement -> agreement.getContract().getName().equals(contractName) ? AgreementValidationResult.SUCCESS : AgreementValidationResult.CONTRACT_IS_NOT_VALID;
+	}
+	
+	static AgreementValidator isServiceValid(String serviceName) {
+		return agreement -> agreement.getService().getName().equals(serviceName) ? AgreementValidationResult.SUCCESS : AgreementValidationResult.SERVICE_IS_NOT_VALID;
+	}
+	
+	default AgreementValidator and(AgreementValidator other) {
+		return agreement -> {
+			AgreementValidationResult result = this.apply(agreement);
+			return result.equals(AgreementValidationResult.SUCCESS) ? other.apply(agreement) : result;
+		};	
+		
+	}
+
+	enum AgreementValidationResult{
+		SUCCESS,
+		CONTRACT_IS_NOT_VALID,
+		SERVICE_IS_NOT_VALID
+	}
+	
+}
