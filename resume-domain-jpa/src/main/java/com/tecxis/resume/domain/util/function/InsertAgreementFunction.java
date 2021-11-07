c97b6c42145919118bee0b29c8ce70ebb8b552f1
Arturo <Arturo@LAPTOP-ASUS>
2021-11-07 11:51:05 +0100

    Adds missing @FunctionalInterface

--- resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/InsertAgreementFunction.java
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/InsertAgreementFunction.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.tecxis.resume.domain.SchemaUtils;
 import com.vladmihalcea.book.hpjp.util.transaction.JPATransactionVoidFunction;
 
+@FunctionalInterface
 public interface InsertAgreementFunction <K> extends JPATransactionVoidFunction<K> {
 	
 	default void beforeTransactionCompletion(JdbcTemplate jdbcTemplateProxy) {
