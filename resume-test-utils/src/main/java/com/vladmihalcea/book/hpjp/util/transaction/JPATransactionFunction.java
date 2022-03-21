8dea30200b022b54e374bf431bcdfa4ed58cb582
Arturo <Arturo@host.docker.internal>
2022-03-21 15:15:15 +0100

    [Function] add Javadoc to transactional functions

--- resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
+++ resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 import javax.persistence.EntityManager;
 
 /**
+ * Similarly to the {@link java.util.function.Function Function} this is a function that accepts one argument and produces a result.
  * @author Vlad Mihalcea
  */
 @FunctionalInterface
--- resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionVoidFunction.java
+++ resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionVoidFunction.java
@@ -3,6 +3,7 @@ package com.vladmihalcea.book.hpjp.util.transaction;
 import java.util.function.Consumer;
 
 /**
+ * Similarly to the {@link java.util.function.Consumer Consumer} specification represents an operation that accepts a single input argument and returns no result. Unlike most other functional interfaces, Consumer is expected to operate via side-effects.
  * @author Vlad Mihalcea
  */
 @FunctionalInterface
