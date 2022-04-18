54eac16668dd1a223dca3af8051c4ee33a60619a
Arturo Medecigo <amt.market1@gmail.com>
2022-04-18 19:14:11 +0200

    [City] Ref. testSetLocations() & JpaDao testSave_UpdateLocation()
    Uses SetCityLocationsFunction <EntityManager> &
    <CityRepository>
    Ref. of CountryValidator.areCitiesValid(List<City> cities) to use
    Collections
    Impl. of CityValidator.areLocationsValid(List <Location> locations)
    Refactor code to use the same same functional validation logic across
    the unit tests.

--- resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/CityValidator.java
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/CityValidator.java
@@ -1,11 +1,14 @@
 package com.tecxis.resume.domain.util.function;
 
 import static com.tecxis.resume.domain.util.function.ValidationResult.CITY_IS_NOT_VALID;
+import static com.tecxis.resume.domain.util.function.ValidationResult.CITY_LOCATIONS_ARE_NOT_VALID;
 import static com.tecxis.resume.domain.util.function.ValidationResult.SUCCESS;
 
+import java.util.List;
 import java.util.function.Function;
 
 import com.tecxis.resume.domain.City;
+import com.tecxis.resume.domain.Location;
 
 @FunctionalInterface
 public interface CityValidator extends Function<City, ValidationResult>  {
@@ -16,5 +19,9 @@ public interface CityValidator extends Function<City, ValidationResult>  {
 	
 	static CityValidator isCountryValid(String countryName) {
 		return city -> city.getCountry().getName().equals(countryName)? SUCCESS : CITY_IS_NOT_VALID;
+	}
+
+	static CityValidator areLocationsValid(List <Location> locations) {
+		return city -> locations.containsAll(city.getLocations()) ? SUCCESS : CITY_LOCATIONS_ARE_NOT_VALID;
 	}
 }
--- resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/CountryValidator.java
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/CountryValidator.java
@@ -4,7 +4,7 @@ import static com.tecxis.resume.domain.util.function.ValidationResult.COUNTRY_CI
 import static com.tecxis.resume.domain.util.function.ValidationResult.COUNTRY_NAME_IS_NOT_VALID;
 import static com.tecxis.resume.domain.util.function.ValidationResult.SUCCESS;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 import com.tecxis.resume.domain.City;
@@ -16,9 +16,9 @@ public interface CountryValidator extends Function<Country, ValidationResult> {
 	static CountryValidator isNameValid(String countryName) {
 		return country -> country.getName().equals(countryName)? SUCCESS : COUNTRY_NAME_IS_NOT_VALID;
 	}
-	
-	static CountryValidator areCitiesValid(City... cities) {
-		return country ->  Arrays.asList(cities).containsAll(country.getCities()) ? SUCCESS : COUNTRY_CITIES_ARE_NOT_VALID;
+
+	static CountryValidator areCitiesValid(List<City> cities) {
+		return country ->  cities.containsAll(country.getCities()) ? SUCCESS : COUNTRY_CITIES_ARE_NOT_VALID;
 	}
 
 }
