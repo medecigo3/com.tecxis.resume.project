6d50d838d8f80b14759be5d25269d56d753f0840
Arturo Medecigo <amt.market1@gmail.com>
2022-04-24 15:19:42 +0200

    [vladmihalcea] Makes JPATransactionFunction EntityManager generic param
    JPATransactionFunction -> function that accepts one argument and
    produces a result.

--- resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
+++ resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
@@ -1,14 +1,13 @@
 package com.vladmihalcea.book.hpjp.util.transaction;
 
 import java.util.function.Function;
-import javax.persistence.EntityManager;
 
 /**
  * Similarly to the {@link java.util.function.Function Function} this is a function that accepts one argument and produces a result.
  * @author Vlad Mihalcea
  */
 @FunctionalInterface
-public interface JPATransactionFunction<T> extends Function<EntityManager, T> {
+public interface JPATransactionFunction<K, T> extends Function<K, T> {
 	default void beforeTransactionCompletion() {
 
 	}
