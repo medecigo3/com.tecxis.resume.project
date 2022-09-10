409706ef6dc9992e075abb11f0fd3ef891bfdf69
Arturo Medecigo <amt.market1@gmail.com>
2022-09-10 10:44:25 +0200

    [SupplierTest] Amends missing SQL unit test inserts

--- resume-domain-jpa/src/test/java/com/tecxis/resume/domain/SupplierTest.java
+++ resume-domain-jpa/src/test/java/com/tecxis/resume/domain/SupplierTest.java
@@ -389,6 +389,9 @@ public class SupplierTest {
 	}
 	
 	@Test
+	@Sql(
+			scripts= {"classpath:SQL/H2/DropResumeSchema.sql", "classpath:SQL/H2/CreateResumeSchema.sql", "classpath:SQL/InsertResumeData.sql" },
+			executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
 	public void test_OneToMany_GetEmploymentContracts() {
 		/**Find target Supplier to test*/
 		Supplier accenture = supplierRepo.getSupplierByName(ACCENTURE_SUPPLIER);
