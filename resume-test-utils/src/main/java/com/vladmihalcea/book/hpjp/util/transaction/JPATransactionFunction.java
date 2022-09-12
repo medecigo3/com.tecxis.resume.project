28b5aa6edf1d4b566517eea5e2b9241b70e96ffb
Arturo Medecigo <amt.market1@gmail.com>
2022-09-12 12:07:24 +0200

    [BUGFIX] Invocation to DeleteContractFunction.before/afterTransaction...
    Completion() with JDBCTemplate.
    +JPATransactionFunction.before/afterTransactionCompletion() - Amended
    ERROR logs when invoked
    +JPATransactionVoidFunction.before/afterTransactionCompletion() -
    Amended ERROR logs when invoked

--- resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
+++ resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionFunction.java
@@ -2,6 +2,8 @@ package com.vladmihalcea.book.hpjp.util.transaction;
 
 import java.util.function.Function;
 
+import org.jboss.logging.Logger;
+
 /**
  * Similarly to the {@link java.util.function.Function Function} this is a function that accepts one argument and produces a result.
  * @author Vlad Mihalcea
@@ -9,10 +11,10 @@ import java.util.function.Function;
 @FunctionalInterface
 public interface JPATransactionFunction<K, T> extends Function<K, T> {
 	default void beforeTransactionCompletion() {
-
+		Logger.getLogger(this.getClass()).error("Nothing to test after transaction complete!!!");
 	}
 
 	default void afterTransactionCompletion() {
-
+		Logger.getLogger(this.getClass()).error("Nothing to test after transaction complete!!!");
 	}
 }
--- resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionVoidFunction.java
+++ resume-test-utils/src/main/java/com/vladmihalcea/book/hpjp/util/transaction/JPATransactionVoidFunction.java
@@ -2,6 +2,8 @@ package com.vladmihalcea.book.hpjp.util.transaction;
 
 import java.util.function.Consumer;
 
+import org.jboss.logging.Logger;
+
 /**
  * Similarly to the {@link java.util.function.Consumer Consumer} specification represents an operation that accepts a single input argument and returns no result. Unlike most other functional interfaces, Consumer is expected to operate via side-effects.
  * @author Vlad Mihalcea
@@ -9,10 +11,10 @@ import java.util.function.Consumer;
 @FunctionalInterface
 public interface  JPATransactionVoidFunction <K> extends Consumer<K> {
 	default void beforeTransactionCompletion() {
-
+		Logger.getLogger(this.getClass()).error("Nothing to test before transaction complete!!!");
 	}
 
 	default void afterTransactionCompletion() {
-
+		Logger.getLogger(this.getClass()).error("Nothing to test after transaction complete!!!");
 	}
 }
