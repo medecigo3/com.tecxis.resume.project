3226f349bc2f079945c95033c2397a6ad729020a
Arturo Medecigo <amt.market1@gmail.com>
2022-09-28 13:43:28 +0200

    [Location][EM][JPA]Impl. updateLocation() test removeOrphansWithOrm

---
+++ resume-domain-jpa/src/main/java/com/tecxis/resume/domain/util/function/SetLocationFunction.java
@@ -0,0 +1,18 @@
+package com.tecxis.resume.domain.util.function;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.tecxis.resume.domain.SchemaUtils;
+import com.vladmihalcea.book.hpjp.util.transaction.JPATransactionVoidFunction;
+
+public interface SetLocationFunction <K> extends JPATransactionVoidFunction<K> {
+	
+	default void beforeTransactionCompletion(JdbcTemplate jdbcTemplateProxy) {
+		SchemaUtils.testInitialState(jdbcTemplateProxy);
+	}
+	
+	default void afterTransactionCompletion(JdbcTemplate jdbcTemplateProxy) {
+		SchemaUtils.testStateAfterMorningstartV1ProjectLocationDelete(jdbcTemplateProxy);
+	}
+
+}
