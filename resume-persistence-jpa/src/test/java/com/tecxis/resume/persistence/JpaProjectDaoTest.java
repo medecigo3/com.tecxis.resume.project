81d6f181949bb14e47351612019077205b6d347c
Arturo Medecigo <amt.market1@gmail.com>
2022-11-03 21:07:43 +0100

    [Utils] Rename & refactor "remove" to "delete" utils methods

--- resume-persistence-jpa/src/test/java/com/tecxis/resume/persistence/JpaProjectDaoTest.java
+++ resume-persistence-jpa/src/test/java/com/tecxis/resume/persistence/JpaProjectDaoTest.java
@@ -5,18 +5,54 @@ import static com.tecxis.resume.domain.Constants.ADIR;
 import static com.tecxis.resume.domain.Constants.AXELTIS;
 import static com.tecxis.resume.domain.Constants.BARCLAYS;
 import static com.tecxis.resume.domain.Constants.BELFIUS;
+import static com.tecxis.resume.domain.Constants.CITY_PARIS_TOTAL_LOCATIONS;
+import static com.tecxis.resume.domain.Constants.CLIENT_ARVAL_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_AXELTIS_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_EULER_HERMES_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_HERMES_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_LA_BANQUE_POSTALE_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_MICROPOLE_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_SAGEMCOM_ID;
+import static com.tecxis.resume.domain.Constants.CLIENT_SG_ID;
 import static com.tecxis.resume.domain.Constants.EOLIS;
 import static com.tecxis.resume.domain.Constants.EULER_HERMES;
+import static com.tecxis.resume.domain.Constants.FRANCE;
+import static com.tecxis.resume.domain.Constants.FRANCE_ID;
 import static com.tecxis.resume.domain.Constants.MORNINGSTAR;
+import static com.tecxis.resume.domain.Constants.PARIS;
+import static com.tecxis.resume.domain.Constants.PARIS_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_AOS_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_CENTRE_DES_COMPETENCES_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_EOLIS_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_EUROCLEAR_VERS_CALYPSO_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_MORNINGSTAR_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_MORNINGSTAR_V2_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_PARCOURS_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_SELENIUM_V1_ID;
+import static com.tecxis.resume.domain.Constants.PROJECT_TED_V1_ID;
 import static com.tecxis.resume.domain.Constants.SAGEMCOM;
 import static com.tecxis.resume.domain.Constants.SHERPA;
+import static com.tecxis.resume.domain.Constants.STAFF_AMT_ID;
+import static com.tecxis.resume.domain.Constants.TASK22_ID;
+import static com.tecxis.resume.domain.Constants.TASK23_ID;
+import static com.tecxis.resume.domain.Constants.TASK24_ID;
+import static com.tecxis.resume.domain.Constants.TASK25_ID;
+import static com.tecxis.resume.domain.Constants.TASK26_ID;
+import static com.tecxis.resume.domain.Constants.TASK27_ID;
+import static com.tecxis.resume.domain.Constants.TASK28_ID;
+import static com.tecxis.resume.domain.Constants.TASK29_ID;
+import static com.tecxis.resume.domain.Constants.TASK30_ID;
+import static com.tecxis.resume.domain.Constants.TASK31_ID;
 import static com.tecxis.resume.domain.Constants.TED;
 import static com.tecxis.resume.domain.Constants.VERSION_1;
 import static com.tecxis.resume.domain.Constants.VERSION_2;
+import static com.tecxis.resume.domain.util.Utils.deleteParisMorningstarV1AxeltisLocationInJpa;
+import static com.tecxis.resume.domain.util.function.ValidationResult.SUCCESS;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTable;
 
 import java.util.List;
@@ -40,10 +76,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tecxis.resume.domain.Assignment;
+import com.tecxis.resume.domain.City;
 import com.tecxis.resume.domain.Client;
+import com.tecxis.resume.domain.Location;
 import com.tecxis.resume.domain.Project;
 import com.tecxis.resume.domain.SchemaConstants;
+import com.tecxis.resume.domain.SchemaUtils;
+import com.tecxis.resume.domain.id.AssignmentId;
+import com.tecxis.resume.domain.id.CityId;
+import com.tecxis.resume.domain.id.LocationId;
+import com.tecxis.resume.domain.id.ProjectId;
+import com.tecxis.resume.domain.repository.AssignmentRepository;
+import com.tecxis.resume.domain.repository.CityRepository;
 import com.tecxis.resume.domain.repository.ClientRepository;
+import com.tecxis.resume.domain.repository.LocationRepository;
 import com.tecxis.resume.domain.repository.ProjectRepository;
 import com.tecxis.resume.domain.util.Utils;
 
@@ -69,6 +116,15 @@ public class JpaProjectDaoTest {
 	
 	@Autowired
 	private ProjectRepository projectRepo;
+	
+	@Autowired
+	private AssignmentRepository assignmentRepo;
+	
+	@Autowired
+	private LocationRepository locationRepo;
+	
+	@Autowired
+	private CityRepository cityRepo;
 
     @AfterClass
     public static void afterClass() {
@@ -216,5 +272,95 @@ public class JpaProjectDaoTest {
 	@Test
 	public void test_OneToMany_SaveLocations() {
 		org.junit.Assert.fail("TODO");
+	}
+	
+	@Test
+	@Sql(
+		scripts= {"classpath:SQL/H2/DropResumeSchema.sql", "classpath:SQL/H2/CreateResumeSchema.sql", "classpath:SQL/InsertResumeData.sql" },
+		executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
+	public void test_OneToMany_DeleteLocation_by_City() {
+		/**Find and validate Project to test*/
+		Project morningstartV1Project = projectRepo.findByNameAndVersion(MORNINGSTAR, VERSION_1);
+		assertNotNull(morningstartV1Project);
+		/**Fetch target Project -> locations*/
+		ProjectId morningstarv1ProjectId = new ProjectId(PROJECT_MORNINGSTAR_V1_ID, CLIENT_AXELTIS_ID);		
+		/**Fetch target Project -> assignments*/
+		Assignment assignment22 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK22_ID)).get();
+		Assignment assignment23 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK23_ID)).get();
+		Assignment assignment24 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK24_ID)).get();
+		Assignment assignment25 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK25_ID)).get();
+		Assignment assignment26 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK26_ID)).get();
+		Assignment assignment27 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK27_ID)).get();
+		Assignment assignment28 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK28_ID)).get();
+		Assignment assignment29 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK29_ID)).get();
+		Assignment assignment30 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK30_ID)).get();
+		Assignment assignment31 =  assignmentRepo.findById(new AssignmentId(morningstarv1ProjectId, STAFF_AMT_ID, TASK31_ID)).get();
+		List <Assignment> morningstarv1AxeltisAssignments = List.of (assignment22,
+				assignment23,
+				assignment24,
+				assignment25,
+				assignment26,
+				assignment27,
+				assignment28,
+				assignment29,
+				assignment30,
+				assignment31);
+		/**Fetch & validate city to remove from project*/
+		City paris = cityRepo.getCityByName(PARIS);
+		/**Fetch 'paris' locations*/
+		Location parisSagemcomTedV1Location = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_TED_V1_ID, CLIENT_SAGEMCOM_ID))).get();
+		Location parisParcoursV1MicropoleLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_PARCOURS_V1_ID, CLIENT_MICROPOLE_ID))).get();
+		Location parisEuroclearV1LbpLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_EUROCLEAR_VERS_CALYPSO_V1_ID, CLIENT_LA_BANQUE_POSTALE_ID))).get();	
+		Location parisMorningstarV1AxeltisLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_MORNINGSTAR_V1_ID, CLIENT_AXELTIS_ID))).get();
+		Location parisEolisV1EhLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_EOLIS_V1_ID, CLIENT_EULER_HERMES_ID))).get();
+		Location parisMorningstarV2AxeltisLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_MORNINGSTAR_V2_ID, CLIENT_AXELTIS_ID))).get();
+		Location parisCdcV1SgLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_CENTRE_DES_COMPETENCES_V1_ID, CLIENT_SG_ID))).get();
+		Location parisAosv1ArvalLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_AOS_V1_ID, CLIENT_ARVAL_ID))).get();
+		Location parisSeleniumV1HermesLocation = locationRepo.findById(new LocationId(new CityId(PARIS_ID,FRANCE_ID), new ProjectId(PROJECT_SELENIUM_V1_ID, CLIENT_HERMES_ID))).get();			
+		List <Location> morningstarv1AxeltisLocations = List.of(parisSagemcomTedV1Location, 
+				parisParcoursV1MicropoleLocation, 	
+				parisEuroclearV1LbpLocation,		
+				parisMorningstarV1AxeltisLocation,
+				parisEolisV1EhLocation,	
+				parisMorningstarV2AxeltisLocation,
+				parisCdcV1SgLocation,
+				parisAosv1ArvalLocation,
+				parisSeleniumV1HermesLocation );
+		assertEquals(CITY_PARIS_TOTAL_LOCATIONS, morningstarv1AxeltisLocations.size());
+		/**Validate City before test*/
+		assertEquals(SUCCESS, Utils.isCityValid(paris, PARIS, FRANCE, morningstarv1AxeltisLocations));
+		/**Fetch target Project -> client*/
+		Client axeltis = clientRepo.getClientByName(AXELTIS);
+		/**Validate target project before test*/
+		assertEquals(SUCCESS, Utils.isProjectValid(morningstartV1Project,  MORNINGSTAR, VERSION_1, morningstarv1AxeltisLocations, axeltis, morningstarv1AxeltisAssignments));
+		
+		/**Remove Project location by city*/	
+		deleteParisMorningstarV1AxeltisLocationInJpa(deleteLocationFuntion -> {			
+			assertTrue(paris.removeLocation(morningstartV1Project));
+			assertTrue(morningstartV1Project.removeLocation(paris));		
+			SchemaUtils.testInitialState(jdbcTemplateProxy);
+			entityManager.merge(morningstartV1Project);
+			entityManager.merge(paris);
+			entityManager.flush();
+			entityManager.clear();
+			
+		}, projectRepo, jdbcTemplateProxy);
+		
+		/**Validate City after test*/
+		morningstarv1AxeltisLocations = List.of(parisSagemcomTedV1Location, 
+				parisParcoursV1MicropoleLocation, 	
+				parisEuroclearV1LbpLocation,
+				parisEolisV1EhLocation,	
+				parisMorningstarV2AxeltisLocation,
+				parisCdcV1SgLocation,
+				parisAosv1ArvalLocation,
+				parisSeleniumV1HermesLocation );
+		assertEquals(CITY_PARIS_TOTAL_LOCATIONS - 1, morningstarv1AxeltisLocations.size()); //1 location removed
+		City newParis = cityRepo.getCityByName(PARIS);
+		assertEquals(SUCCESS, Utils.isCityValid(newParis, PARIS, FRANCE, morningstarv1AxeltisLocations));
+		/**Validate target  project after test*/
+		Project newMorningstarV1 = projectRepo.findByNameAndVersion(MORNINGSTAR, VERSION_1);
+		assertNotNull(newMorningstarV1);
+		assertEquals(SUCCESS, Utils.isProjectValid(newMorningstarV1,  MORNINGSTAR, VERSION_1, morningstarv1AxeltisLocations, axeltis, morningstarv1AxeltisAssignments));
 	}
 }
