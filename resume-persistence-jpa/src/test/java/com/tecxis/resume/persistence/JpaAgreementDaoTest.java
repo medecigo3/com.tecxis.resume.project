4d31e35ace3fdae14a0dc51d1847ef799d7a941b
Arturo Medecigo <amt.market1@gmail.com>
2022-11-07 20:27:59 +0100

    UpdateAgreementServiceFunction renamed to SetAgreementServiceFunction

--- resume-persistence-jpa/src/test/java/com/tecxis/resume/persistence/JpaAgreementDaoTest.java
+++ resume-persistence-jpa/src/test/java/com/tecxis/resume/persistence/JpaAgreementDaoTest.java
@@ -123,7 +123,7 @@ public class JpaAgreementDaoTest {
 		
 		/**Find new Contract to set in Agreement*/
 		Contract accentureBarclaysContract = contractRepo.getContractByName(CONTRACT1_NAME);
-		Utils.setAgreementContractInJpa(UpdateAgreementContractFunction-> {
+		Utils.setAgreementContractInJpa(SetAgreementServiceFunction-> {
 			/***Create new Agreement*/
 			AgreementId newAxeltisFastConnectAgreementId = new AgreementId();
 			newAxeltisFastConnectAgreementId.setContractId(accentureBarclaysContract.getId()); //set new contract id
